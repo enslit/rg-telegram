80befc6d9d0a5f1d4a82ff0e9e092738a0b031c4
Alexander Emelyanov <Alexaneder.Emelyanov@kaspersky.com>
2024-02-12 06:54:56 +0300

    init

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, {
+    logger: ['log', 'warn', 'error'],
+  });
+
+  await app.listen(5000);
+}
+bootstrap();
